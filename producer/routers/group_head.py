eff4901729913ccb8f6760dfefcee7323f407e5d
qnichi21999 <21999german@gmail.com>
2025-04-12 22:26:51 +0300

    Make route for reserve_slot endpoint

--- producer/routers/group_head.py
+++ producer/routers/group_head.py
@@ -1,18 +1,23 @@
 from fastapi import APIRouter, Depends, Request
 
-from producer.core.dependencies import role_checker
+from producer.core.dependencies import get_current_user, role_checker
 from producer.core.rabbitmq import rabbitmq_manager
 from producer.core.exceptions import InternalServerError
 
 router = APIRouter(prefix="/group_head", tags=["group_head"])
 
-@router.get("/slot")
-async def reserve_slot(request: Request, current_user: dict = Depends(role_checker("group_head"))):
+@router.post("/reserve_slot")
+async def reserve_slot(request: Request, weekday: int, schedule_item_type: str, slot_start_time: str, current_user: dict = Depends(get_current_user)):
+    if current_user["role"] != "group_head":
+        raise ForbiddenError("Access allowed only for group_head")
     try:
         message = {
             "action": "reserve_slot",
             "data": {
-                "user_id": current_user["id"]
+                "user_id": current_user["id"],
+                "weekday": weekday,
+                "schedule_item_type": schedule_item_type,
+                "slot_start_time": slot_start_time
             }
         }
 
